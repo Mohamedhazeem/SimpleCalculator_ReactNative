9c3de698580bd22400dec06be6db110b58ff2f03
Mohamed Hazeem <a.mohamedhazeem@gmail.com>
2024-03-06 19:28:43 +0530

    Create Basics

---
+++ components/Button.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+export interface ButtonProps {
+  value: string;
+  onPress: () => void;
+  isEqualButton?: boolean;
+}
+
+export const Button = ({value, onPress, isEqualButton}: ButtonProps) => {
+  return (
+    <View style={isEqualButton ? styles.equalButton : styles.button}>
+      <Text onPress={onPress} style={styles.buttonText}>
+        {value}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'black',
+  },
+  equalButton: {
+    backgroundColor: 'orangered',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    padding: 10,
+  },
+});
---
+++ components/Row.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {ButtonProps, Button} from './Button';
+
+interface RowProps {
+  buttons: ButtonProps[];
+}
+
+const Row = ({buttons}: RowProps) => {
+  return (
+    <View style={styles.row}>
+      {buttons.map(button => (
+        <Button
+          value={button.value}
+          onPress={button.onPress}
+          isEqualButton={button.isEqualButton}
+        />
+      ))}
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+});
+
+export default Row;
